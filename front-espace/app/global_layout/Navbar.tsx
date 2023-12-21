69fed6572a3584d3d0860031cc42060090c68c3f
Jean Choppin de Janvry <jean.choppin-de-janvry@student-cs.fr>
2023-12-21 01:29:08 +0100

    getting a navbar

--- front-espace/app/global_layout/Navbar.tsx
+++ front-espace/app/global_layout/Navbar.tsx
@@ -1,22 +1,21 @@
-
 import React from 'react';
 
 const Navbar: React.FC = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/PoleEspace">Pôle Espace</a>
-        </li>
-        <li>
-          <a href="/PoleHelics">Pôle HéliCS</a>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', backgroundColor: 'darkblue', padding: '1rem', position: 'sticky', top: 0 }}>
+            <ul style={{ display: 'flex', listStyleType: 'none', padding: 0 }}>
+                <li style={{ marginRight: '1rem' }}>
+                    <a href="/">Home</a>
+                </li>
+                <li style={{ marginRight: '1rem' }}>
+                    <a href="/PoleEspace">Pôle Espace</a>
+                </li>
+                <li style={{ marginRight: '1rem' }}>
+                    <a href="/PoleHelics">Pôle HéliCS</a>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
